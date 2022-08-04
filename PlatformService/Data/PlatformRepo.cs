305480f7552192e762e6aac85069918cd79adf0d
danyelchidera <danyelchidera1@gmail.com>
2022-08-05 00:48:13 +0100

    Add project files.

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,30 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+                throw new ArgumentNullException(nameof(platform));
+            _context.Add(platform);
+
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms() =>
+            _context.Platforms.ToList();
+
+        public Platform GetPlatformById(int id) =>
+            _context.Platforms.FirstOrDefault(x => x.Id.Equals(id));
+
+        public bool SaveChanges() =>
+            _context.SaveChanges() > 0;
+    }
+}
